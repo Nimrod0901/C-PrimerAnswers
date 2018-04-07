c677e71ea648d1427b9829129ab78662c770779c
Nimrod0901 <nimrodcowboy@gmail.com>
2018-04-07 10:19:56 +0800

    Add 11_38

---
+++ ch11/11_38.cpp
@@ -0,0 +1,20 @@
+#include <unordered_map>
+#include <string>
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::unordered_map;
+using std::cin;
+using std::size_t;
+
+int main() {
+	unordered_map<string, size_t> word_count;
+	string w;
+	for(string w; cin >> w; ++word_count[w]);
+	for(const auto &w : word_count) {
+		cout << w.first << " occurs " << w.second << ((w.second > 1) ? " times" : " time") << endl;
+	}
+	return 0;
+}
