ee7ce317b7d49169ac064610af4cd442a05be53b
Nimrod0901 <nimrodcowboy@gmail.com>
2018-05-04 18:40:47 +0800

    Change the form

--- ch12/12_02.h
+++ ch12/12_02.h
@@ -1,5 +1,6 @@
 #ifndef STRBLOB_H
 #define STRBLOB_H
+
 #include <memory>
 #include <string>
 #include <vector>
@@ -10,25 +11,31 @@ public:
     StrBlob() : data(std::make_shared<std::vector<std::string>>()){};
     StrBlob(std::initializer_list<std::string> il)
         : data(std::make_shared<std::vector<std::string>>(il)) {}
+        
     size_type size() const { return data->size(); }
     bool empty() const { return data->empty(); }
     void push_back(const std::string& t) { data->push_back(t); }
+
     void pop_back() {
         check(0, "pop_back on empty StrBlob");
         data->pop_back();
     }
+
     std::string& front() {
         check(0, "front on empty StrBlob");
         return data->front();
     }
+
     std::string& back() {
         check(0, "back on empty StrBlob");
         return data->back();
     }
+
     const std::string& front() const {
         check(0, "front on empty StrBlob");
         return data->front();
     }  // overload
+    
     const std::string& back() const {
         check(0, "back on empty StrBlob");
         return data->back();
